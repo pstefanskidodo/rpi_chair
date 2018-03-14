00755209c39ada69bd659cd034b7169ae12394a1
p.stefanski <p.stafanski@games.dodo4story.com>
2018-03-14 15:07:36 +0100

    Keyboard input added

--- tcp_client/tcp_client_test.py
+++ tcp_client/tcp_client_test.py
@@ -2,11 +2,14 @@ import socket
 import os
 import time
 import sys
+from curtsies import Input
+
 from uuid import getnode
+
 
 TCP_IP='192.168.0.111'
 TCP_PORT=56789
-BUFFER_SIZE=2048
+BUFFER_SIZE=1024
 
 h = iter(hex(getnode())[2:].zfill(12))
 mac_address = "".join(i+next(h) for i in h)
@@ -38,18 +41,31 @@ if connected:
 	rep_mess = "@1;" + id + ";" + mac_address + ";"
 	print(rep_mess)
 	s.send(rep_mess.encode("utf-8"))
-	#time.sleep(2)
 	data = ''
-	while True:
-		try:
-			print(s.recv(BUFFER_SIZE).decode("utf-8").split(";"))
-			#data = s.recv(BUFFER_SIZE).split(";")
-			#if data[0]=="@3":
-			#	rot=float(data[2])
-			#	print(rot)
-			#	s.send("@4;" + str(0) + ";" + str(rot) + ";" + str(0))
-		except KeyboardInterrupt:
-			break
+	reactor = Input(keynames='curses')
+	sttr=''
+	with reactor:
+		while True:
+			try:
+				e=Input(keynames='curses').send(0.05)
+				left = False
+				right = False
+				up = False
+				down = False
+				#print(s.recv(BUFFER_SIZE).decode("utf-8").split(";"))
+				if repr(e) == "'KEY_LEFT'":
+					left = True
+				if repr(e) == "'KEY_RIGHT'":
+					right = True
+				if repr(e) == "'KEY_UP'":
+					up = True
+				if repr(e) == "'KEY_DOWN'":
+					down = True
+				sttr = "@4;" + str(id) + ";" + "connected" + ";" + str(10) + ";" + str(down) + ";" + str(right) + ";" + str(left) + ";" + str(up) + ";"
+				s.send(sttr.encode("utf-8"))
+				print(sttr)
+			except KeyboardInterrupt:
+				break
 exit_mess = "@6;" + id + ";"
 s.send(exit_mess.encode("utf-8"))
 s.close()
